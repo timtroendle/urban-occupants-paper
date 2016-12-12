6d80c3a2d70775b049d93c6c1d65645b853c5965
Tim Tröndle <tt397@cam.ac.uk>
2016-12-12 15:25:29 +0000

    Add Python setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# People
+
+An activity and location model of urban citizens.
+
+## Developer Guide
+
+### Installation
+
+Best install `people` in editable mode:
+
+    $ pip install -e .
+
+### Run the test suite
+
+Run the test suite with py.test:
+
+    $ py.test
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+exec(open('people/__init__.py').read())
+
+setup(
+    name='people',
+    version=__version__,
+    description='An activity and location model of urban citizens.',
+    maintainer='Tim Tröndle',
+    maintainer_email='tt397@cam.ac.uk',
+    url='https://www.github.com/timtroendle/people',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering'
+    ]
+)
