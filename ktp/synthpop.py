30ba19cb9f5e735f629c780b2945ed8e62b1a974
Tim Tröndle <tt397@cam.ac.uk>
2017-02-07 16:31:28 +0000

    Add handling of more than one category
    Add a test example from mlipf R package and made sure it return
    correct results. The results are ok, but not very precise and also
    do not match the ones from mlipf. Needs more work.

--- ktp/synthpop.py
+++ ktp/synthpop.py
@@ -43,15 +43,16 @@ def fit_hipf(reference_sample, controls_individuals, controls_households, maxite
         data=1.0,
         dtype=np.float64
     )
-    for i in range(0, maxiter):
+    for i in range(1, maxiter + 1):
         next_weights = _fit(_household_groups(reference_sample).first(), weights,
                             controls_households)
         weights_person = _expand_weights_to_person(next_weights, reference_sample.index)
         weights_person = _fit(reference_sample, weights_person, controls_individuals)
         next_weights = _aggregate_person_weights_to_household(weights_person)
-        next_weights = _rescale_weights(reference_sample, next_weights)
-        previous_weights = weights
-        weights = next_weights
+        next_weights = _rescale_weights(reference_sample, next_weights,
+                                        controls_individuals, controls_households)
+        previous_weights = weights.copy()
+        weights = next_weights.copy()
         if (residuals_tol is not None and
             _residuals_tolerance_reached(reference_sample, weights, controls_households,
                                          controls_individuals, residuals_tol)):
@@ -70,6 +71,11 @@ def _household_groups(reference_sample):
 
 def _residuals_tolerance_reached(reference_sample, weights, controls_households,
                                  controls_individuals, tol):
+    residuals = _all_residuals(reference_sample, weights, controls_households, controls_individuals)
+    return residuals.abs().max() < tol
+
+
+def _all_residuals(reference_sample, weights, controls_households, controls_individuals):
     household_ref_sample = _household_groups(reference_sample).first()
     residuals_household = _residuals(
         reference_sample=_household_groups(reference_sample).first(),
@@ -81,8 +87,7 @@ def _residuals_tolerance_reached(reference_sample, weights, controls_households,
         weights=_expand_weights_to_person(weights, reference_sample.index),
         controls=controls_individuals
     )
-    residuals = pd.Series(list(chain(residuals_household, residuals_individual)))
-    return residuals.abs().max() < tol
+    return pd.Series(list(chain(residuals_household, residuals_individual)))
 
 
 def _residuals(reference_sample, weights, controls):
@@ -109,11 +114,11 @@ def _grand_total(controls):
 def _fit(reference_sample, weights, controls):
     new_weights = weights.copy()
     for control_name, control_values in controls.items():
-        summed_weights = {key: weights[reference_sample[control_name] == key].sum()
+        summed_weights = {key: new_weights[reference_sample[control_name] == key].sum()
                           for key, value in control_values.items()}
         control_values = reference_sample[control_name].map(control_values)
         summed_weights = reference_sample[control_name].map(summed_weights)
-        new_weights = weights * control_values / summed_weights
+        new_weights = new_weights * control_values / summed_weights
     return new_weights
 
 
@@ -131,17 +136,20 @@ def _expand_weights_to_person(weights, person_index):
     return person_weights.iloc[:, 0] # return series not dataframe
 
 
-def _rescale_weights(reference_sample, weights):
+def _rescale_weights(reference_sample, weights, controls_individuals, controls_households):
+    grand_total_hh = _grand_total(controls_households)
+    grand_total_ind = _grand_total(controls_individuals)
     household_sizes = _household_groups(reference_sample)[reference_sample.columns[0]].count()
     largest_household_size = household_sizes.max()
     Fp = [weights[household_sizes == p].sum()
           for p in range(0, largest_household_size + 1)]
-    polynom = [(190 / 434 * p - 1) * Fp[p] for p in range(0, largest_household_size + 1)]
+    polynom = [(grand_total_hh / grand_total_ind * p - 1) * Fp[p]
+               for p in range(0, largest_household_size + 1)]
     roots = Polynomial(polynom).roots()
     dx = list(filter(lambda x: np.real(x) > 0, filterfalse(lambda x: np.iscomplex(x), roots)))
     assert len(dx) == 1
     d = np.real(dx[0])
-    c = 190 / sum(Fp[p] * d ** p for p in range(1, largest_household_size + 1))
+    c = grand_total_hh / sum(Fp[p] * d ** p for p in range(1, largest_household_size + 1))
     fhprime_by_fh = {p: c * d ** p for p in range(1, largest_household_size + 1)}
 
     fhprime_by_fh = household_sizes.map(fhprime_by_fh)
---
+++ tests/test_hipf_with_two_controls.py
@@ -0,0 +1,80 @@
+"""These tests test the HIPF algorithm with two control totals.
+
+The problem is the `Separate` problem among the toy example taken from
+https://github.com/krlmlr/MultiLevelIPF/commit/408d23d9db6536b47d78ab0c65aff40111a829fc.
+
+Results were created from the given repository with default parameters, i.e. running
+`ml_fit_hipf(toy_example('Separate'))`. Hence `tol` was 1e-6, `diff_tol` was set to
+2.220446e-16, and `maxiter` was 200.
+"""
+import sys
+import math
+from pathlib import Path
+
+import pandas as pd
+from pandas.util.testing import assert_series_equal
+import pytest
+
+sys.path.append('.')
+from ktp.synthpop import fit_hipf, _all_residuals
+
+
+RESOURCES_PATH = Path(__file__).parent / 'resources'
+PATH_TO_REFERENCE_SAMPLE = RESOURCES_PATH / 'two_controls_reference_sample.csv'
+PATH_TO_RESULTS = RESOURCES_PATH / 'two_controls_results.csv'
+
+
+@pytest.fixture
+def reference_sample():
+    sample = pd.read_csv(PATH_TO_REFERENCE_SAMPLE)
+    return sample.set_index(['HHNR', 'PNR'])
+
+
+@pytest.fixture
+def controls_individuals():
+    return {'WKSTAT': {0: 395, 1: 459}, 'GENDER': {'X': 434, 'Y': 420}}
+
+
+@pytest.fixture
+def controls_households():
+    return {'CAR': {0: 99, 1: 273}}
+
+
+@pytest.fixture
+def expected_weights():
+    weights = pd.read_csv(PATH_TO_RESULTS)
+    return weights.reset_index(drop=True).iloc[:, 0] # weights are indexed from 1, ref sample from 0
+
+
+def assert_weights_equal(expected_weights, weights, precision=1):
+    weights.name = expected_weights.name # don't want to check the name
+    weights.index.name = expected_weights.index.name # don't want to check the name
+    assert_series_equal(expected_weights, weights, check_less_precise=precision)
+
+
+@pytest.mark.xfail(reason="Currently the algorithms do not seem to be equal.")
+def test_same_result_like_mlipf(reference_sample, expected_weights, controls_individuals,
+                                controls_households):
+    weights = fit_hipf(
+        reference_sample=reference_sample,
+        controls_individuals=controls_individuals,
+        controls_households=controls_households,
+        weights_tol=2.220446e-16,
+        residuals_tol=1e-6,
+        maxiter=200
+    )
+    assert_weights_equal(expected_weights, weights)
+
+
+@pytest.mark.parametrize("tol", [(1), pytest.mark.xfail(0.1)])
+def test_converges(reference_sample, controls_households, controls_individuals, tol):
+    weights = fit_hipf(
+        reference_sample=reference_sample,
+        controls_individuals=controls_individuals,
+        controls_households=controls_households,
+        weights_tol=2.220446e-16,
+        residuals_tol=1e-6,
+        maxiter=200
+    )
+    residuals = _all_residuals(reference_sample, weights, controls_households, controls_individuals)
+    assert residuals.abs().max() < tol
